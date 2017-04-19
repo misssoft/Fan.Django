c7d60d711e7e6313957236666b6bc5a0ca89cf16
Fan Yang <fan.yang.turner@live.co.uk>
2017-04-19 22:26:47 +0100

    Add model, migration, db, admin

--- ukschools/main/models.py
+++ ukschools/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class School (models.Model):
+    name = models.CharField(max_length = 250)
+    phase = models.CharField(max_length = 50)
+    type = models.CharField(max_length = 50)
+    ofsted = models.CharField(max_length = 50)
+    ofstedInspected = models.DateTimeField(auto_now=False)
+    religious = models.CharField(max_length = 50)
+    gender = models.CharField(max_length = 20)
+    address = models.CharField(max_length = 100)
+    postcode = models.CharField(max_length = 10)
