cc19511cf0095bb6cf182b0c3608a97fe627094e
Fan Yang <fan.yang.turner@live.co.uk>
2017-04-20 22:22:42 +0100

    Read data from db to home page

--- ukschools/main/views.py
+++ ukschools/main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import School
 
 # Create your views here.
 def home(request):
-    return render(request,"main/home.html", {'message':'Hi, Django!'})
+    all = School.objects.all()
+    primary = all.filter(phase = "P")
+    secondary = all.filter(phase = "S")
+    alevel = all.filter(phase = "A")
+    context = {
+        'primary': primary,
+        'secondary':secondary,
+        'alevel':alevel
+    }
+    return render(request,"main/home.html", context)
